d2fcdcbb82e47d069c1bbb93a74b7e245339b303
Steffi <stefanie.mak@web.de>
2022-10-14 15:38:22 +0200

    feat: Add new data to existing dataframe
    New function in Data class adds new row to existing data.
    New row is instance of new function in NewData, data_to_series.

--- scripts/main.py
+++ scripts/main.py
@@ -5,10 +5,16 @@ class Data:
     
     def __init__(self, filename):
         self.filename = filename
+        self.df = self.open_file()
 
     def open_file(self):
         data_frame = pd.read_csv(self.filename)
         return data_frame
+
+    def add_new_data_to_df(self, new_data):
+        self.df = pd.concat([self.df,
+                            new_data.data_to_series()],
+                            ignore_index=True)
 
 
 class NewData:
@@ -17,17 +23,23 @@ class NewData:
         self.date = date
         self.temperature = temperature
 
+    def data_to_series(self):
+        return pd.DataFrame({"date": [self.date],
+                             "temperature": [self.temperature]})
+
     def __repr__(self):
         return f"Temperature on {self.date}: {self.temperature} °C"
 
 
 if __name__ == "__main__":
-    # my_date = input("What is the date? ")
-    # my_temperature = float(input("What is the temperature? "))
-    # new_data = NewData(my_date, my_temperature)
+    my_date = "2022-10-14"  # input("What is the date? ")
+    my_temperature = 37.5  # float(input("What is the temperature? "))
+    new_data = NewData(my_date, my_temperature)
+
     my_filename = "../data/test_data.csv"
     my_data = Data(filename=my_filename)
-    print(my_data.open_file())
+    my_data.add_new_data_to_df(new_data)
+    print(my_data.df)
 
 
     
